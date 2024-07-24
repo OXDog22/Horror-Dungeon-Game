4ecaf2cd8115fd5c68d52187203cabbbe97db914
abreeana <abreeana2007@gmail.com>
2024-07-24 14:04:57 -0700

    script

--- Group Project/Group Horror Game Project/Assets/Scripts/PlayerScript.cs
+++ Group Project/Group Horror Game Project/Assets/Scripts/PlayerScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = Time.deltaTime;
     }
 
     // Update is called once per frame
