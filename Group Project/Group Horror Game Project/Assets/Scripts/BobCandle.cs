c822c42a8556a6c6530cb574c787c7cd620fcf3c
HuskyWolf76890 <ethan.r.zwicker@gmail.com>
2024-07-25 09:44:47 -0700

    Update BobCandle.cs

--- Group Project/Group Horror Game Project/Assets/Scripts/BobCandle.cs
+++ Group Project/Group Horror Game Project/Assets/Scripts/BobCandle.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BobCandle : MonoBehaviour
 {
+    private Vector3 offSet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offSet = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //bobs the candle
+        transform.position += transform.right * Mathf.Cos(Time.time * 2f) /256;
+        transform.position += transform.up * Mathf.Cos(Time.time * 2f) / -512;
     }
 }
