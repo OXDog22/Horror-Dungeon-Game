77cc4e9314d27a32279fec6865550f753681d949
abreeana <abreeana2007@gmail.com>
2024-07-26 10:06:42 -0700

    level

--- Group Project/Group Horror Game Project/Assets/Scripts/NextLevelSpawner.cs
+++ Group Project/Group Horror Game Project/Assets/Scripts/NextLevelSpawner.cs
@@ -5,15 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelSpawner : MonoBehaviour
 {
+
+    private string Name = "Level Castle Scene";
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        NextLevel();
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(Name);
     }
 }
