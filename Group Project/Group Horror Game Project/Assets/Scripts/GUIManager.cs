6dfb124d18b2081d6c468c814333fe5db35995a2
Sugarkitty81 <Victoriarooks37@gmail.com>
2024-07-26 14:40:16 -0700

    Update GUIManagerScript.cs

--- Group Project/Group Horror Game Project/Assets/Scripts/GUIManager.cs
+++ Group Project/Group Horror Game Project/Assets/Scripts/GUIManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GUIManager : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    private GameObject playerCamera;
+    public UnityEngine.UI.Button ArrowLeft;
+    public UnityEngine.UI.Button ArrowRight;
+    public GameObject Objectpt1;
+    public GameObject Objectpt2;
+    public GameObject Objectpt3;
+    private UnityEngine.UI.Text TitleName;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +28,12 @@ public class GUIManager : MonoBehaviour
     public void ButtonTest()
     {
         print("We did it!");
+    }
+    public void MoveAround()
+    {
+        playerRb.transform.rotation = playerCamera.transform.rotation;
+        Objectpt1.transform.rotation = playerCamera.transform.rotation;
+        Objectpt2.transform.rotation = playerCamera.transform.rotation;
+        Objectpt3.transform.rotation = playerCamera.transform.rotation;
     }
 }
