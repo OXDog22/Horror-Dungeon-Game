1026da44a22d9bce52b7506e1fbe072e303c6ad4
HuskyWolf76890 <ethan.r.zwicker@gmail.com>
2024-07-30 13:26:26 -0700

    Animated Blood

--- Group Project/Group Horror Game Project/Assets/Scripts/EnemyScript.cs
+++ Group Project/Group Horror Game Project/Assets/Scripts/EnemyScript.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
     private float speed = 2;
     private Rigidbody enemyRb;
     private GameObject player;
+    private PlayerScript PlayerScript;
     public float enemySensingDistance = 5;
     public float enemyChaseRange = 10;
     public float enemyPounceRange = 2;
     private Animator enemyAnimator;
     private int movementEnabled = 1;
+    private bool hiding;
+    public GameObject animatedBlood;
+    private bool hitCoolDown = false;
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
-        player = GameObject.Find("Cylinder");
+        player = GameObject.Find("Player");
+        PlayerScript= player.GetComponent<PlayerScript>();
         enemyAnimator = gameObject.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -27,8 +34,10 @@ public class EnemyScript : MonoBehaviour
         
         Vector3 playerDirection = new Vector3(player.transform.position.x - transform.position.x, 0, player.transform.position.z - transform.position.z);
         
+        hiding = PlayerScript.hiding;
+
           //Chases the player if it is too close or exceeds the chase range and is not already pouncing/ has pounced
-        if ((Mathf.Abs(playerDirection.x) < enemySensingDistance && Mathf.Abs(playerDirection.z) < enemySensingDistance || (enemyAnimator.GetBool("isChasing") && Mathf.Abs(playerDirection.x) < enemyChaseRange && Mathf.Abs(playerDirection.z) < enemyChaseRange ) && !enemyAnimator.GetBool("Pounce")))
+        if ((Mathf.Abs(playerDirection.x) < enemySensingDistance && Mathf.Abs(playerDirection.z) < enemySensingDistance || (enemyAnimator.GetBool("isChasing") && Mathf.Abs(playerDirection.x) < enemyChaseRange && Mathf.Abs(playerDirection.z) < enemyChaseRange ) && !enemyAnimator.GetBool("Pounce")) && !hiding)
         {
             
             if (Mathf.Abs(playerDirection.x) < enemyPounceRange && Mathf.Abs(playerDirection.z) < enemyPounceRange)
@@ -66,4 +75,33 @@ public class EnemyScript : MonoBehaviour
         movementEnabled = 0;
     // for the sake of types, 0 = false, 1 = true
     }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //player takes damage
+        if (other.CompareTag("Player") && !hiding && !hitCoolDown)
+        {
+            PlayerScript.Hp -= 1;
+
+            animatedBlood.SetActive(true);
+            hitCoolDown= true;
+            StartCoroutine(HurtEffects());
+         
+        }
+
+        
+    }
+
+    IEnumerator HurtEffects()
+    {
+        yield return new WaitForSeconds(3);
+        animatedBlood.SetActive(false);
+        hitCoolDown = false;
+
+    }
+
+    
 }
