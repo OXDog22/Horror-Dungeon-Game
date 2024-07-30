170b75db654e2677a2de0cddf8f72e9530781fc7
Sugarkitty81 <Victoriarooks37@gmail.com>
2024-07-30 14:44:43 -0700

    update GUIManagerScript.cs

--- Group Project/Group Horror Game Project/Assets/Scripts/GUIManager.cs
+++ Group Project/Group Horror Game Project/Assets/Scripts/GUIManager.cs
@@ -6,12 +6,21 @@ public class GUIManager : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject playerCamera;
-    public UnityEngine.UI.Button ArrowLeft;
-    public UnityEngine.UI.Button ArrowRight;
-    public GameObject Objectpt1;
-    public GameObject Objectpt2;
-    public GameObject Objectpt3;
     private UnityEngine.UI.Text TitleName;
+
+    // These are our Buttons
+    public UnityEngine.UI.Button arrowLeft;
+    public UnityEngine.UI.Button arrowRight;
+    public UnityEngine.UI.Button exitButton;
+    public UnityEngine.UI.Button inventoryButton;
+
+    // these are our Inventory items
+    public GameObject objectpt1;
+    public GameObject objectpt2;
+    public GameObject objectpt3;
+
+    // Lets us know when Inventory is open
+    private bool isInventoryOpen = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,13 +38,54 @@ public class GUIManager : MonoBehaviour
     {
         print("We did it!");
     }
-    public void MoveAround()
-    {
-        playerRb.transform.rotation = playerCamera.transform.rotation;
-        Objectpt1.transform.rotation = playerCamera.transform.rotation;
-        Objectpt2.transform.rotation = playerCamera.transform.rotation;
-        Objectpt3.transform.rotation = playerCamera.transform.rotation;
+    public void OpenInventory()
 
-        Instantiate(Objectpt1);
+     // Makes inventory items appear when Inventory opened
+    {   if (isInventoryOpen == false)
+
+        {
+            Instantiate(objectpt1);
+            Instantiate(objectpt2);
+            Instantiate(objectpt3);
+            Instantiate(arrowRight);
+            Instantiate(arrowLeft);
+            Instantiate(exitButton);
+            isInventoryOpen = true;
+        }
+
+        playerRb.transform.rotation = playerCamera.transform.rotation;
+        objectpt1.transform.rotation = playerCamera.transform.rotation;
+        objectpt2.transform.rotation = playerCamera.transform.rotation;
+        objectpt3.transform.rotation = playerCamera.transform.rotation;
+
+        Destroy(inventoryButton);
+
+    }
+
+    public void CloseInventory()
+
+     // Make items dissapear when Inventory closed
+    {
+        if (isInventoryOpen == true)
+        {
+            Destroy(objectpt1);
+            Destroy(objectpt2);
+            Destroy(objectpt3);
+            Destroy(arrowRight);
+            Destroy(arrowLeft);
+            Destroy(exitButton);
+            isInventoryOpen = false;
+        }
+
+    }
+
+    public void LeftButton()
+    {
+
+    }
+
+    public void RightButton()
+    {
+
     }
 }
