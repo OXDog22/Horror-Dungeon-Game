808351688f50c33cdf09115036525f530e1fa126
HuskyWolf76890 <ethan.r.zwicker@gmail.com>
2024-08-01 10:59:35 -0700

    Title screen now works!

---
+++ Group Project/Group Horror Game Project/Assets/Scripts/BackStoryManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Timeline;
+using UnityEngine.SceneManagement;
+using System.Xml.Linq;
+
+public class BackStoryManager : MonoBehaviour
+{
+
+    public GameObject backStoryCanvas;
+    public TextMeshProUGUI storyText;
+    public string[] story;
+    private string Name = "Intro Scene";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGameBackstory()
+    {
+        backStoryCanvas.SetActive(true);
+        StartCoroutine(DisplayText());
+
+    }
+
+    IEnumerator DisplayText()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            storyText.text = story[i];
+            yield return new WaitForSeconds(1);
+            yield return new WaitUntil(CheckSpaceKeyPressed);
+        }
+
+        SceneManager.LoadScene(Name);
+    }
+
+    private bool CheckSpaceKeyPressed()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+}
