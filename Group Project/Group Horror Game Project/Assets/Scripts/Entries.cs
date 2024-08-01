ce7e19bce70e2de4ff596667842dc54e53ffdb27
HuskyWolf76890 <ethan.r.zwicker@gmail.com>
2024-08-01 12:45:50 -0700

    Entries

---
+++ Group Project/Group Horror Game Project/Assets/Scripts/Entries.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entries : MonoBehaviour
+{
+    public string noteText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //make it reference entryText, display in GUI and add it to the inventory
+}
